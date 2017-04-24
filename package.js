9e5b12eaf5042f813c7b00e97377e877deed63a7
Mitar <mitar.git@tnode.com>
2017-04-23 19:59:54 -0700

    Version bump.

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'peerlibrary:publish-context',
   summary: "Provide DDP._CurrentPublish with access to current publish context",
-  version: '0.2.0',
+  version: '0.3.0',
   git: 'https://github.com/peerlibrary/meteor-publish-context.git'
 });
 
