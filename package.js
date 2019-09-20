1ae986111d4cc9496445c3e1eb29cb72949007b7
Mitar <mitar.git@tnode.com>
2019-09-19 19:16:26 -0700

    Version bump.

--- package.js
+++ package.js
@@ -1,23 +1,23 @@
 Package.describe({
   name: 'peerlibrary:publish-context',
   summary: "Provide DDP._CurrentPublish with access to current publish context",
-  version: '0.6.0',
+  version: '0.7.0',
   git: 'https://github.com/peerlibrary/meteor-publish-context.git'
 });
 
 Package.onUse(function (api) {
-  api.versionsFrom('METEOR@1.4.4.5');
+  api.versionsFrom('METEOR@1.8.1');
 
   // Core dependencies.
   api.use([
-    'coffeescript@2.0.3_3',
+    'coffeescript@2.4.1',
     'ecmascript',
     'ddp'
   ], 'server');
 
   // 3rd party dependencies.
   api.use([
-    'peerlibrary:extend-publish@0.5.0'
+    'peerlibrary:extend-publish@0.6.0'
   ], 'server');
 
   api.addFiles([
